2f089c41b4d4bdd83865e9049e4d67121e200c7f
Dsturchi <youremail@salohiddinayyubiiyy@gmail.com>
2022-03-12 20:22:57 +0500

    metod

--- login_password.py
+++ login_password.py
@@ -1,3 +1,19 @@
 class User:
-    print("salom")
-    pass
+    def __init__(self):
+        pass
+
+    def registor(self):
+        print("salom")
+        pass
+
+    def login(self):
+        print("salom")
+        pass
+
+    def log_out(self):
+        print("salom")
+        pass
+
+    def change_pass(self):
+        print("salom")
+        pass
